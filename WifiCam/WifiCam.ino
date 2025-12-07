cb8246e7f99cd0e1312484ccea668de8af6cb0f7
D22CQAT01-B <b22dcat145@gmail.com>
2025-12-07 12:52:26 +0700

    Update WifiCam.ino

--- WifiCam/WifiCam.ino
+++ WifiCam/WifiCam.ino
@@ -4,25 +4,16 @@
 static const char* WIFI_SSID = "HuyNQ";
 static const char* WIFI_PASS = "huyandwho";
 
-// ==========================
-// GPIO ESP32-CAM
-// ==========================
 #define LED_BUILTIN 4
 #define RELAY_PIN 2
 
-// ===========================
-// Biến logic
-// ===========================
 boolean activeRelay = false;
 unsigned long prevMillis = 0;
-int interval = 5000; // mở cửa 5 giây
+int interval = 5000;
 
 esp32cam::Resolution initialResolution;
 WebServer server(80);
 
-// ===========================
-// Hàm xử lý unlock
-// ===========================
 void handleUnlock() {
   if (!activeRelay) {
     activeRelay = true;
@@ -30,57 +21,40 @@ void handleUnlock() {
     digitalWrite(LED_BUILTIN, HIGH);
     
     prevMillis = millis();
-    
-    Serial.println("✔ UNLOCK DOOR!");
-    
+    Serial.println("UNLOCK DOOR!");
     server.send(200, "text/plain", "Door unlocked");
   } else {
     server.send(200, "text/plain", "Already unlocked");
   }
 }
 
-// ===========================
-// Hàm xử lý lock thủ công
-// ===========================
 void handleLock() {
   if (activeRelay) {
     activeRelay = false;
     digitalWrite(RELAY_PIN, LOW);
     digitalWrite(LED_BUILTIN, LOW);
-    
-    Serial.println("✖ LOCK DOOR");
-    
+    Serial.println("LOCK DOOR");
     server.send(200, "text/plain", "Door locked");
   } else {
     server.send(200, "text/plain", "Already locked");
   }
 }
 
-// ===========================
-// Hàm kiểm tra trạng thái
-// ===========================
 void handleStatus() {
   String status = activeRelay ? "unlocked" : "locked";
   server.send(200, "text/plain", status);
 }
 
-// ===========================
-// SETUP
-// ===========================
 void setup() {
   Serial.begin(115200);
   Serial.println();
   esp32cam::setLogger(Serial);
   delay(1000);
-
-  // --- GPIO ---
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
-  
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
-  // --- WIFI ---
   WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
@@ -93,8 +67,6 @@ void setup() {
   
   Serial.println("WiFi connected");
   delay(1000);
-
-  // --- CAMERA ---
   {
     using namespace esp32cam;
 
@@ -118,10 +90,8 @@ void setup() {
   Serial.print("http://");
   Serial.println(WiFi.localIP());
 
-  // --- WEB SERVER ---
-  addRequestHandlers();  // Handlers từ WifiCam library
+  addRequestHandlers();
   
-  // Thêm các endpoint mới cho relay
   server.on("/unlock", HTTP_GET, handleUnlock);
   server.on("/lock", HTTP_GET, handleLock);
   server.on("/status", HTTP_GET, handleStatus);
@@ -135,19 +105,12 @@ void setup() {
   Serial.println("  GET /status - Kiểm tra trạng thái");
 }
 
-// ===========================
-// LOOP
-// ===========================
 void loop() {
   server.handleClient();
-  
-  // Tự động đóng cửa sau interval
   if (activeRelay && millis() - prevMillis > interval) {
     activeRelay = false;
-    
     digitalWrite(RELAY_PIN, LOW);
-    digitalWrite(LED_BUILTIN, LOW);
-    
+    digitalWrite(LED_BUILTIN, LOW); 
     Serial.println("✖ AUTO LOCK DOOR");
   }
 }
